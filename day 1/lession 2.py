1eccaab9a871300890dc254e801faf4fb805395c
datomuxi <datomuxi@gmail.com>
2023-12-14 01:10:47 +0400

    second commit

---
+++ day 1/lession 2.py
@@ -0,0 +1,9 @@
+name ="dato"
+
+
+surname ="muxigulashvili"
+
+age =20
+height =191.5
+print("my name is " + "dato" + " " + "and my surname is " + "muxigulashvili" + " " "and my age is " + str("20") + " " "my height is " + str("191.5"))
+      
